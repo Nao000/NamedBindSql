2652a995a9f22cf600ed98d34c2628cc3331397b
Nao000 <info@nao000.com>
2021-04-12 00:05:34 +0900

    modify function scope

--- lib/named_bind_sql.ex
+++ lib/named_bind_sql.ex
@@ -1,29 +1,29 @@
 defmodule NamedBindSql do
   def prepare_sql_with_params(sql, bind_map) do
     remove_duplicate_colon_word = sql
-    |> only_colon_word()
-    |> remove_duplicate_colon_word()
+    |> _only_colon_word()
+    |> _remove_duplicate_colon_word()
 
     list_to_ordered_map = remove_duplicate_colon_word
-    |> list_to_ordered_map()
+    |> _list_to_ordered_map()
 
     prepare_bind_list = remove_duplicate_colon_word
-    |> prepare_bind_list(bind_map)
+    |> _prepare_bind_list(bind_map)
 
     {replace_with_map(sql, list_to_ordered_map), prepare_bind_list}
   end
 
-  def only_colon_word(sql) do
+  defp _only_colon_word(sql) do
     sql
     |> String.split([" ", ";", "\n"])
     |> Enum.filter(fn(x) -> String.at(x, 0) == ":" end)
   end
 
-  def remove_duplicate_colon_word(colon_word_list) do
+  defp _remove_duplicate_colon_word(colon_word_list) do
     colon_word_list |> Enum.uniq
   end
 
-  def list_to_ordered_map(list) do
+  defp _list_to_ordered_map(list) do
     _list_to_ordered_map(list, %{}, 1)
   end
 
@@ -33,7 +33,7 @@ defmodule NamedBindSql do
     _list_to_ordered_map(rest, Map.put(reduced, first, "$" <> Integer.to_string(number)), number+1)
   end
 
-  def replace_with_map(string, map) do
+  defp replace_with_map(string, map) do
     string
     |> String.replace(";", " ;")
     |> String.replace("\n", " \n")
@@ -46,7 +46,7 @@ defmodule NamedBindSql do
     if doller, do: doller, else: word
   end
 
-  def prepare_bind_list(prepare_list, bind_map) do
+  defp _prepare_bind_list(prepare_list, bind_map) do
     prepare_list
     |> Enum.map(fn prepare_list -> Map.get(bind_map, prepare_list) end)
   end
